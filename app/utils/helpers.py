d20410fe2f135a1cf122503f8388d66822669f03
oxqnd <kkinho04@catholic.ac.kr>
2025-05-20 11:00:00 +0000

    feat : add classifier queue consumer

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -16,6 +16,6 @@ async def send_to_queue(method, props, message):
                 ),
                 routing_key=props["reply_to"],
             )
-            print(f"Message sent to queue '{props['reply_to']}'")
+            print(f"Message sent to queue '{props['reply_to']}'") ###
     except Exception as e:
         print(f"Error sending message to queue: {str(e)}")
